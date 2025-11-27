7e08738af36635428bb6b847002a642e08fb4f1c
PhamTanThanh2005 <tanthanhquanghai@gmail.com>
2025-11-27 14:16:51 +0700

    Done Register function  with Select Tutor

---
+++ src/mockdata/tutorSlots.js
@@ -0,0 +1,56 @@
+const tutorSlots = {
+  1: [
+    { day: "Thứ 2", date: "27/11/2025", week: 48, time: "08:00 - 10:00", available: true, maxSlots: 5, booked: 0 },
+    { day: "Thứ 3", date: "28/11/2025", week: 48, time: "14:00 - 16:00", available: true, maxSlots: 5, booked: 2 },
+    { day: "Thứ 5", date: "30/11/2025", week: 48, time: "09:00 - 11:00", available: false, maxSlots: 5, booked: 5 },
+    { day: "Thứ 6", date: "01/12/2025", week: 49, time: "15:00 - 17:00", available: true, maxSlots: 5, booked: 1 },
+  ],
+
+  2: [
+    { day: "Thứ 2", date: "27/11/2025", week: 48, time: "13:00 - 15:00", available: true, maxSlots: 3, booked: 0 },
+    { day: "Thứ 4", date: "29/11/2025", week: 48, time: "09:00 - 11:00", available: false, maxSlots: 3, booked: 3 },
+  ],
+
+  3: [
+    { day: "Thứ 3", date: "28/11/2025", week: 48, time: "08:00 - 10:00", available: false, maxSlots: 2, booked: 2 },
+    { day: "Thứ 6", date: "01/12/2025", week: 49, time: "09:00 - 11:00", available: false, maxSlots: 2, booked: 2 },
+  ],
+
+  4: [
+    { day: "Thứ 2", date: "27/11/2025", week: 48, time: "09:00 - 11:00", available: true, maxSlots: 4, booked: 1 },
+    { day: "Thứ 4", date: "29/11/2025", week: 48, time: "15:00 - 17:00", available: true, maxSlots: 4, booked: 0 },
+  ],
+
+  5: [
+    { day: "Thứ 3", date: "28/11/2025", week: 48, time: "10:00 - 12:00", available: true, maxSlots: 5, booked: 2 },
+    { day: "Thứ 5", date: "30/11/2025", week: 48, time: "13:00 - 15:00", available: true, maxSlots: 5, booked: 0 },
+    { day: "Thứ 6", date: "01/12/2025", week: 49, time: "09:00 - 11:00", available: false, maxSlots: 5, booked: 5 },
+  ],
+
+  6: [
+    { day: "Thứ 2", date: "27/11/2025", week: 48, time: "08:00 - 10:00", available: false, maxSlots: 3, booked: 3 },
+    { day: "Thứ 3", date: "28/11/2025", week: 48, time: "14:00 - 16:00", available: false, maxSlots: 3, booked: 3 },
+  ],
+
+  7: [
+    { day: "Thứ 4", date: "29/11/2025", week: 48, time: "09:00 - 11:00", available: true, maxSlots: 4, booked: 1 },
+    { day: "Thứ 6", date: "01/12/2025", week: 49, time: "15:00 - 17:00", available: true, maxSlots: 4, booked: 0 },
+  ],
+
+  8: [
+    { day: "Thứ 2", date: "27/11/2025", week: 48, time: "15:00 - 17:00", available: true, maxSlots: 2, booked: 0 },
+    { day: "Thứ 5", date: "30/11/2025", week: 48, time: "08:00 - 10:00", available: false, maxSlots: 2, booked: 2 },
+  ],
+
+  9: [
+    { day: "Thứ 3", date: "28/11/2025", week: 48, time: "13:00 - 15:00", available: true, maxSlots: 3, booked: 1 },
+  ],
+
+  10: [
+    { day: "Thứ 2", date: "27/11/2025", week: 48, time: "09:00 - 11:00", available: true, maxSlots: 5, booked: 2 },
+    { day: "Thứ 4", date: "29/11/2025", week: 48, time: "14:00 - 16:00", available: true, maxSlots: 5, booked: 0 },
+    { day: "Thứ 6", date: "01/12/2025", week: 49, time: "08:00 - 10:00", available: true, maxSlots: 5, booked: 1 },
+  ],
+};
+
+export default tutorSlots;
